5c12f8b0ce259cfedb61f33dd45c8abfb48b87ac
Gatoxy <104008458+gatoxy@users.noreply.github.com>
2024-05-05 14:35:16 -0300

    Update bot.py

--- gemini_pro_bot/bot.py
+++ gemini_pro_bot/bot.py
@@ -21,7 +21,7 @@ load_dotenv()
 def start_bot() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token(os.getenv("6257702674:AAGjdj_HWk3686TlvpWa-phaCwj6J8q0z98")).build()
+    application = Application.builder().token(os.getenv("6246048342:AAEfUGZnJbb3jypRSX_k7HWKtWmZNJTXEGw")).build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start, filters=AuthFilter))
